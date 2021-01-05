e9ddd0e37b2a21817a1d303f35feadabc599d1a9
Shreemun <shreemun@gmail.com>
2021-01-05 11:08:44 +0530

    Section 2, challenge 1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const calcAverage = (n1, n2, n3) => (n1 + n2 + n3) / 3;
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins > avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+    } else {
+        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+    }
+}
+let avgDolphins = calcAverage(85, 54, 41);
+let avgKoalas = calcAverage(23, 34, 27);
+checkWinner(avgDolphins, avgKoalas);
+//Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
